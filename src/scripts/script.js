f0569b5ef8f383616393adcffc3e411688953ce5
tpdtrang <tpdtrang97@gmail.com>
2019-03-21 23:05:37 +0700

    fix wow

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,31 @@
+// counter
+var a = 0;
+$(window).scroll(function () {
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.counter-value').each(function () {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum: countTo
+            },
+                {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+                    },
+                    complete: function () {
+                        $this.text((this.countNum).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+                    }
+
+                });
+        });
+        a = 1;
+    }
+})
 // header
 $(document).ready(function () {
     $(window).scroll(function () {
@@ -6,48 +34,17 @@ $(document).ready(function () {
         if (scroll < 100) {
             opacity = scroll / 100;
             $("#p-page-header").css('background-color', 'rgba(75, 193,210,' + opacity + ')');
-
         }
         else {
             opacity = 1;
             $("#p-page-header").css('background-color', 'rgba(75, 193,210,' + opacity + ')');
         }
     })
-});
-
-//  slider
-$(document).ready(function () {
+ //slider
     $('.p-page-slider').slick({ dots: true, arrows: false, infinite: true, autoplaySpeed: 1000 });
-});
-// counter
-var a = 0;
-$(window).scroll(function() {
 
-  var oTop = $('#counter').offset().top - window.innerHeight;
-  if (a == 0 && $(window).scrollTop() > oTop) {
-    $('.counter-value').each(function() {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-          countNum: countTo
-        },
-        {
-          duration: 2000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
-          }
-        });
-    });
-    a = 1;
-  }
 });
+
 //wow
 var wow = new WOW(
     {
