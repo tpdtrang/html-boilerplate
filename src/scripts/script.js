c4c016a342d4c5a8bf5c0d234f8cffd48347dbd3
tpdtrang <tpdtrang97@gmail.com>
2019-03-24 18:18:04 +0700

    Push feature/fix-master

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
             $("#p-page-header").css('background-color', 'rgba(75, 193,210,' + opacity + ')');
         }
     })
- //slider
+    //slider
     $('.p-page-slider').slick({ dots: true, arrows: false, infinite: true, autoplaySpeed: 1000 });
 
 });
@@ -48,18 +48,18 @@ $(document).ready(function () {
 //wow
 var wow = new WOW(
     {
-      boxClass:     'wow',      // animated element css class (default is wow)
-      animateClass: 'animated', // animation css class (default is animated)
-      offset:       0,          // distance to the element when triggering the animation (default is 0)
-      mobile:       true,       // trigger animations on mobile devices (default is true)
-      live:         true,       // act on asynchronously loaded content (default is true)
-      callback:     function(box) {
-        // the callback is fired every time an animation is started
-        // the argument that is passed in is the DOM node being animated
-      },
-      scrollContainer: null,    // optional scroll container selector, otherwise use window,
-      resetAnimation: true,     // reset animation on end (default is true)
+        boxClass: 'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset: 0,          // distance to the element when triggering the animation (default is 0)
+        mobile: true,       // trigger animations on mobile devices (default is true)
+        live: true,       // act on asynchronously loaded content (default is true)
+        callback: function (box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null,    // optional scroll container selector, otherwise use window,
+        resetAnimation: true,     // reset animation on end (default is true)
     }
-  );
-  wow.init();
-  
+);
+wow.init();
+
